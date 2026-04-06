6323484ed9c2e043c7f3ab25394732fadf5f533f
Akmal Azizov <62462485+akmal199601@users.noreply.github.com>
2026-04-06 11:13:22 +0500

    create task number 5 and push him

--- task_number_5.py
+++ task_number_5.py
@@ -1,9 +1,20 @@
-from playwright.async_api import async_playwright, Page
+from playwright.sync_api import Page, expect
 
 from main import BASE_URL
 
 
-def test_checkboxes(page :Page) -> None:
-    page.goto(BASE_URL) + "/checkboxes"
-    checkboxes  = page.locator("#checkboxes input")
-    check1 = che
+def test_checkboxes(page: Page):
+
+    checkboxes = page.locator("#checkboxes input")
+    page.goto(BASE_URL + "/checkboxes")
+
+
+    check1 = checkboxes.nth(0)
+    check2 = checkboxes.nth(1)
+
+    expect(check1).not_to_be_checked()
+    expect(check2).to_be_checked()
+    check1.check()
+    check2.uncheck()
+    print(f"✅ Checkbox 1: checked={check1.is_checked()}")
+    print(f"✅ Checkbox 2: checked={check2.is_checked()}")
